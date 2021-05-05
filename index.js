8a430d80579bbbb8878f36a560f1390884ac0dde
nameless <nameless_developer@protonmail.com>
2021-05-05 15:07:42 -0400

    Implemented novel caching

--- index.js
+++ index.js
@@ -2,11 +2,12 @@ import puppeteer from 'puppeteer'
 import fs from 'fs'
 // import promptSync from'prompt-sync'
 // const prompt = promptSync()
+import {readAsync} from './lib/util.js'
 
 import clipboardy from 'clipboardy'
 
 
-async function main(url){
+async function main(url) {
   const browser = await puppeteer.launch()
   try {
     await app(browser, url)
@@ -23,14 +24,72 @@ if(url){
   main(url)
 }
 
-async function app(browser, url){
+const MILOVANA_CACHE = '/tmp/milovana'
+
+async function app(browser, url) {
   const page = await browser.newPage()
   await page.setViewport({ width: 1000, height: 1000 })
   await page.goto(url, {
     waitUntil: 'networkidle2',
   })
   
+  if (!fs.existsSync(MILOVANA_CACHE)){
+    fs.mkdirSync(MILOVANA_CACHE);
+  }
+  const title = await getTitle(page)
+  const novel = await getNovel(page, `${MILOVANA_CACHE}/${title}.md`)
 
+  await debugScreenshot(page, title)
+
+  clipboardy.writeSync(novel);
+}
+
+function format(text, src) {
+  // reduce whitespace
+  const txt = text.replace(/\n\n\n/g, '\n');
+  // markdown image format
+  const img = `![](${src})`
+  return `${txt}\n${img}\n\n`
+}
+
+
+async function getNovel(page, path) {
+  const cache = await queryCache(path)
+  // Cache hit
+  if(cache !== null){ 
+    return cache
+  }
+  // scrape and cache!
+  const novel = await getMilovanaNovel(page)
+  fs.writeFileSync(path, novel);
+  return novel
+}
+
+async function queryCache(path) {
+  const exists = fs.existsSync(path)
+  if (exists){
+    const file = await readAsync(path)
+    console.log(file)
+    return file
+  }
+  return null
+}
+
+// Its actually ${title} by ${author}, which makes it a good uuid too. But whatever
+async function getTitle(page) {
+  const titleNode = await page.$('#tease_title')
+  return await titleNode.evaluate(el => el.textContent)
+}
+
+async function debugScreenshot(page, title) {
+  const path = `${MILOVANA_CACHE}/debug-${title}.png`
+  if (fs.existsSync(path)){
+    fs.unlinkSync(path)
+  }
+  await page.screenshot({ path })
+}
+
+async function getMilovanaNovel(page) {
   let continueButton
   let novel = ''
   do {
@@ -51,22 +110,5 @@ async function app(browser, url){
     }
   } while(continueButton !== null)
 
-  const titleNode = await page.$('#tease_title')
-  let title = await titleNode.evaluate(el => el.textContent)
-  const tmpDir = '/tmp/milovana'
-  if (!fs.existsSync(tmpDir)){
-    fs.mkdirSync(tmpDir);
-  }
-  const path = `${tmpDir}/debug-${title}.png`
-  fs.unlinkSync(path)
-  await page.screenshot({ path })
-  clipboardy.writeSync(novel);
-}
-
-function format(text, src){
-  // reduce whitespace
-  const txt = text.replace(/\n\n\n/g, '\n');
-  // markdown image format
-  const img = `![](${src})`
-  return `${txt}\n${img}\n\n`
-}
+  return novel
+} 

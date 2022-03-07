6ea3b2cd59b3468a2f1dcb4b463343293bc7a819
nameless <nameless_developer@protonmail.com>
2022-03-07 16:37:15 -0500

    bunch of improvements

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 import puppeteer from 'puppeteer'
 import clipboardy from 'clipboardy'
 import { isWebUri } from 'valid-url'
 import { Cache } from './lib/cache.js'
 import { getScraper } from './lib/scrapers/index.js'
 
-const E_SCRAPER_CACHE = '/tmp/e-scraper'
+import { config } from 'dotenv'
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+config({ path: __dirname + '/.env' });
+
+
+const E_SCRAPER_CACHE = process.env.E_SCRAPER_CACHE || '/tmp/e-scraper';
 
 const urls = process.argv.slice(2)
 
@@ -15,13 +23,13 @@ async function main(urls) {
   try {
     if (urls.length === 0) {
       const clipboard = clipboardy.readSync()
-      if(clipboard.startsWith('https://')){
+      if (clipboard.startsWith('https://')) {
         await app(browser, clipboard)
       } else {
         process.exit(0)
       }
     }
-    
+
     if (urls.length === 1) {
       await app(browser, urls[0])
     } else {
@@ -38,7 +46,7 @@ async function main(urls) {
 }
 
 async function app(browser, url, silent = false) {
-  if(!isWebUri(url)) {
+  if (!isWebUri(url)) {
     console.error('Not a valid URL')
     return
   }
@@ -47,7 +55,7 @@ async function app(browser, url, silent = false) {
   const cache = new Cache(E_SCRAPER_CACHE, silent)
 
   const Scraper = getScraper(url)
-  const scraper = new Scraper({page, cache})
+  const scraper = new Scraper({ page, cache })
   await scraper.scrape(url)
 
   await page.close()
